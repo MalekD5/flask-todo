a213b707b0913518d9fa607f8a498492aafd1735
MalekD5 <106095300+MalekD5@users.noreply.github.com>
2024-07-31 00:16:24 +0300

    [feat]: init flask login and fix import

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,9 +1,19 @@
 import os
 from flask import (Flask, render_template)
 from infrastructure import TodoRepository as td
-from app.controller import todo_controller
+from app.routes import todo_controller
+from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.secret_key = os.getenv("SECRET_KEY")
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
 
 app.config["LIVETW_DEV"] = os.getenv("LIVETW_ENV") == "development"
 
